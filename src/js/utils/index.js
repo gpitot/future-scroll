a4f1ed9229e3bce54109d063e68b56bdcea33733
guillaume <gpitot@nine.com.au>
2019-11-03 16:46:50 +1100

    comment

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,5 +1,7 @@
 
 //math
+
+// map number x from range [a, b] to [c, d]
 const map = (x, a, b, c, d) => (x - a) * (d - c) / (b - a) + c;
 const lerp = (a, b, n) => (1 - n) * a + n * b;
 const getRandomFloat = (min, max) => (Math.random() * (max - min) + min).toFixed(2);
